812d2534671e4a9c37566942b859b4e36b790dfb
Free5Dev <saidsoumah@gmail.com>
2018-05-07 14:31:07 +0200

    mofication cours lynda JS

--- cours/lynda/Chapitre04DOM/04-03/script.js
+++ cours/lynda/Chapitre04DOM/04-03/script.js
@@ -1,11 +1,14 @@
+// reperage d'élément grace aux dom
 var mainTitle=document.getElementById('mainTitle');
 console.log(mainTitle);//pour recuperer mainTitle dans js et le manipuler comme bon le semble
-console.log("Cet element est de type:"+mainTitle.nodeType);//pour connaitre le type de noeud cad element noeud
-console.log("Cet element à "+mainTitle.childNodes.length+" noeuds enfants");//nombre de noeuds enfants
-
-var links=document.getElementsByTagName('a');// le total des liens
-console.log("il ya "+links.length+" liens dans cette page ");
-
-var homeNav=document.getElementById("homeNav");
-var linksBar=homeNav.getElementsByTagName('a');
-console.log('il ya '+linksBar.length+" liens dans homeNav");
+// le type d'element du node ave note type
+console.log("Cet élément est de type:"+mainTitle.nodeType+" cad un element node");//pour connaitre le type de noeud cad element noeud
+// pour afficher le nombre de node enfants
+console.log("Cet element à:"+mainTitle.childNodes.length+" nodes enfants");//pour connaitre le nombre de nodes enfants
+// reperage de plusieurs elements grace aux noms de la balise utilisés dans l'elements....
+var links=document.getElementsByTagName('a');//pour recuperer le nombre de lien dans la page
+console.log(" il ya:"+links.length+" liens dans cette page");//.length pour renvoie le nombre en chiffre sans affiché sous forme de array
+// pour connaitre le nombre de lien dans une section proporementide
+var linsBar=document.getElementById('homeNav');
+var liens=linsBar.getElementsByTagName('a');
+console.log("il ya "+liens.length+" liens dans la page");
--- cours/lynda/Chapitre04DOM/04-04/script.js
+++ cours/lynda/Chapitre04DOM/04-04/script.js
@@ -1,15 +1,14 @@
-var mainContent = document.getElementById("mainContent");
-mainContent.setAttribute("align","right");
-
-mainTitle = document.getElementById("mainTitle");
-mainTitle.innerHTML="autre texte";
-console.log(mainTitle.innerHTML);
-
+//pour modifier l'allignement d'un texte
+var mainContent=document.getElementById('mainContent');//recuperage d'abord du mainContent
+mainContent.setAttribute("align","right");//attribution de l'alignement à droite avec setAttribute
+console.log(mainContent);//affichage de l'alignement.
+//pour modifier le texte
+var mainTitle=document.getElementById('mainTitle');//recuperage d'abord du mainTitle
+mainTitle.innerHTML="Autre texte";
+console.log(mainTitle.innerHTML);//affichage du texte de h1
+// sidebar
 var sidebar = document.getElementById("sidebar");
 console.log(sidebar.innerHTML);
 
 var arrayOfH1s = mainContent.getElementsByTagName("h1");
 arrayOfH1s[0].innerHTML = "Voici un nouveau titre";
-
-
-

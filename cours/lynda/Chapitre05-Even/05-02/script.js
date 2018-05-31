463796d5bab7c87a1bdca82efb415931596a6052
Free5Dev <saidsoumah@gmail.com>
2018-05-31 11:55:31 +0200

    ajout encore event

--- cours/lynda/Chapitre05-Even/05-02/script.js
+++ cours/lynda/Chapitre05-Even/05-02/script.js
@@ -1,14 +1,16 @@
 
 //document.onclick = function() {
-//	alert("Vous avez cliqué dans le document");
+	//alert("Vous avez cliqué dans le document");
 //};
 
-//	var myImage = document.getElementById("mainImage");
-//	myImage.onclick =  function() {
-//		alert("Vous avez cliqué sur l'image");
-//	}
+function prepareEventHandlers(){//on crer une function prepareEventHandlers dans la quelle on met l'event onclick
+	var myImage = document.getElementById("mainImage");
+		myImage.onclick =  function() {
+			alert("Vous avez cliqué sur l'image");
+		}
+}
 
-//window.onload = function() {
+window.onload = function() {//appel de la function quand la page est preparer avec window onload cad quand on lit la page
 	// Préparer ce dont on a besoin
-//	prepareEventHandlers();
-//};
+	prepareEventHandlers();
+};

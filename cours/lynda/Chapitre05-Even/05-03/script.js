7787d5f120135b5b1292ae85727e0d47407a0dc4
Free5Dev <saidsoumah@gmail.com>
2018-05-31 12:26:01 +0200

    les timers

--- cours/lynda/Chapitre05-Even/05-03/script.js
+++ cours/lynda/Chapitre05-Even/05-03/script.js
@@ -1,13 +1,13 @@
 
 var emailField = document.getElementById("email");
 
-emailField.onfocus = function() {
+emailField.onfocus = function() {//quand on rentre dans le champs
 	if ( emailField.value == "Votre e-mail") {
 		emailField.value = "";
 	}
 };
 
-emailField.onblur = function() {
+emailField.onblur = function() {//quand on quitte le champs
 	if ( emailField.value == "") {
 		emailField.value = "Votre e-mail";
 	}
--- cours/lynda/Chapitre05-Even/05-04/script.js
+++ cours/lynda/Chapitre05-Even/05-04/script.js
@@ -1,24 +1,32 @@
 // Deux méthodes pour les timers - setTimeout() et SetInterval() (uique / répétitif)
 
-function simpleMessage() {
+function simpleMessage() {//simple function de creation d'alert
 	alert("Je suis une simple alerte !");
 }
 
 // settimeout est en milisecondes
+//setTimeout(simpleMessage,5000);//pour affiché l'alert simpleMessage quand fait 5000ms sur la page
 
-var myImage = document.getElementById("mainImage");
+var myImage = document.getElementById("mainImage");//recuperage de mainimage du dom
 
 var imageArray = ["_images/overlook.jpg","_images/winery_sign.jpg","_images/lunch.jpg",
-				  "_images/bigSur.jpg","_images/flag_photo.jpg","_images/mission_look.jpg"];
-var imageIndex = 0;
+				  "_images/bigSur.jpg","_images/flag_photo.jpg","_images/mission_look.jpg"];//array qui prend l'adresse des images
+var imageIndex = 0;//variable initalisé à 0
 
-function changeImage() {
-	myImage.setAttribute("src",imageArray[imageIndex]);
-	imageIndex++;
-	if (imageIndex >= imageArray.length) {
+function changeImage() {//function qui change l'image
+	myImage.setAttribute("src",imageArray[imageIndex]);//setAttribute pour affecter l'adresse des images du array
+	imageIndex++;//incrementation de l'index de l'image
+	if (imageIndex >= imageArray.length) {//cdt pour respecter le parcours des images du tableaux
 		imageIndex = 0;
 	}
 }
 
 // setInterval est aussi en milisecondes
-//var imageInterval = setInterval(changeImage,5000);
+var imageInterval = setInterval(changeImage,5000);//pour changé les images du tableau un à un 
+
+myImage.onmouseover=function(){//pour stopper le parcour des images pointées par le curseur
+	clearInterval(imageInterval);//netoie imageInterval
+}
+myImage.onmouseout=function(){
+	imageInterval=setInterval(changeImage,5000);//pour reprendre le parcour des images
+};
